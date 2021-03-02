6378293f3ea1d8ed252443b2fb29c929992f9310
Otavio Verissimo Santos <otavio.santos@stone.com.br>
2021-03-02 20:23:40 -0300

    adds geometric operations.

---
+++ src/CharpEvolution/Tests02/IGeometricOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpEvolution
+{
+    public interface IGeometricOperations
+    {
+        double TriangleArea(double width, double height);
+        double CircleArea(double radius);
+        double SquareArea(double width, double height);
+        double Volume(double area, double lenght, double height);
+    }
+    public class GeometricOperations : IGeometricOperations
+    {
+        public double CircleArea(double radius)
+        {
+            return Math.Round(Math.PI * Math.Pow(radius, 2));
+        }
+
+        public double SquareArea(double width, double height)
+        {
+            return Math.Round(width * height, 2);
+        }
+
+        public double TriangleArea(double width, double height)
+        {
+            return Math.Round((width * height) / 2, 2);
+        }
+
+        public double Volume(double area, double lenght, double height)
+        {
+            return Math.Round(area * lenght * height, 2);
+        }
+    }
+}
